b781bd3b664b70ceddd30ed00d8e640af0578762
thomaskrut <thomas.krut@yh.nackademin.se>
2023-08-25 12:27:39 +0200

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { TheChessboard } from 'vue3-chessboard'
+import 'vue3-chessboard/style.css'
+
+import type { BoardApi, BoardConfig } from 'vue3-chessboard'
+import type { Move } from 'chess.js'
+
+const orientation = ref<BoardConfig["orientation"]>('black')
+
+let boardAPI: BoardApi;
+const boardConfig: BoardConfig = {
+  coordinates: true,
+  orientation: orientation.value,
+}
+
+const pieceMoved = (move: Move) => {
+  console.log('Piece moved', move);
+}
+
+</script>
+
+<template>
+  <TheChessboard
+      :board-config="boardConfig"
+      @board-created="(api) => (boardAPI = api)"
+      @move="(move) => pieceMoved(move)"
+    />
+</template>
+
+<style scoped>
+
+</style>
