f0d82eac170c230f722e1b6843bf345892a716d3
thomaskrut <thomas.krut@yh.nackademin.se>
2023-09-04 14:48:20 +0200

    major refactoring, new gameplay class

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <script setup lang="ts">
-import { ref, watchEffect, computed } from 'vue'
-import { TheChessboard, type MoveEvent } from 'vue3-chessboard'
-import type { Square } from 'chess.js'
+import { ref } from 'vue'
+import { TheChessboard } from 'vue3-chessboard'
 import 'vue3-chessboard/style.css'
+import { BoardApi, type BoardConfig } from 'vue3-chessboard'
+import { GameplayApi } from './types/Gameplay'
 
-import type { BoardApi, BoardConfig } from 'vue3-chessboard'
-import type { Move } from './types/Move'
-import { Turn } from './types/Turn'
-import type { UserFeedback } from './types/UserFeedback'
-import { getTotalNumberOfGames, getRandomMove } from './utils/utils'
+import { getTotalNumberOfGames } from './utils/utils'
+import type { MovesData } from './types/MovesData'
 
-import { useFetchMovesData } from './useFetchMovesData'
-import { Tree } from './types/Tree'
+const orientation = ref<BoardConfig['orientation']>('white')
 
-const orientation = ref<BoardConfig['orientation']>('black')
-const turn = ref<Turn>(new Turn('white'))
-const moveSequence = ref<string>('')
-const movesData = useFetchMovesData(moveSequence)
-const selectedMove = ref<Move | null>(null)
-const movesToConsider = 2
-const userFeedback = ref<UserFeedback>({ message: 'Välj öppningsdrag', color: 'primary', icon: "mdi-information", buttonText: "Välj drag"})
+let board: BoardApi
 
-const tree: Tree = new Tree()
+let gameplay: GameplayApi = new GameplayApi(orientation.value)
 
-let boardAPI: BoardApi;
-
-let submitButtonCallback = submitMove
+const { movesData, userFeedback } = gameplay.useGameplayData()
 
 const boardConfig: BoardConfig = {
   coordinates: true,
@@ -39,101 +28,28 @@ const boardConfig: BoardConfig = {
   },
 }
 
-function pieceMoved(move: MoveEvent) {
-  selectedMove.value = movesData.value!.moves.filter(m => m.san === move.san)[0]
+function initGame(newBoard: BoardApi) {
+  gameplay.setBoard(newBoard)
+  board = newBoard
+  
 }
 
-function drawArrow(move: Move) {
-  const from = move.uci.slice(0, 2) as Square
-  const to = move.uci.slice(2, 4) as Square
-  boardAPI.drawMove(from, to, 'green')
-}
 
-function removeArrows() {
-  boardAPI.hideMoves()
-}
-
-function previewMove(move: Move) {
-  boardAPI.move(move.san)
-  selectedMove.value = move
-}
-
-function resetBoard() {
-  boardAPI.resetBoard()
-  turn.value = new Turn('white')
-  moveSequence.value = ''
-  submitButtonCallback = submitMove
-  userFeedback.value = { message: 'Välj öppningsdrag', color: 'primary', icon: "mdi-information", buttonText: "Välj drag"}
-  tree.resetMoveSequence()
-}
-
-function submitMove() {
-  tree.addMove(selectedMove.value!)
-  moveSequence.value = tree.getMoveSequence()
-  turn.value.toggle()
-  if (playersTurn.value) determineState()
-  console.log(tree)
-}
-
-function submitAndReset() {
-  tree.addMove(selectedMove.value!)
-  resetBoard()
-}
-
-function confirmVariationSaved() {
-  userFeedback.value = { message: 'Variant sparad', color: 'info', icon: "mdi-check", buttonText: "OK"}
-  submitButtonCallback = submitAndReset
-}
-
-function guessMove() {
-  if (tree.hasNextMove(selectedMove.value!)) {
-    userFeedback.value = { message: 'Rätt!', color: 'success', icon: 'mdi-star-face', buttonText: "Fortsätt"}
-    submitButtonCallback = submitMove
-  } else {
-    userFeedback.value = { message: 'Fel! Rätt drag var...', color: 'error', icon: 'mdi-alert', buttonText: "Fortsätt"}
-    submitButtonCallback = resetBoard
-  }
-}
-
-function determineState() {
-  if (tree.hasMoves()) {
-    userFeedback.value = { message: 'Vilket drag spelar du nu?', color: 'info', icon: 'mdi-head-question', buttonText: "Välj drag"}
-    submitButtonCallback = guessMove
-  } else {
-    userFeedback.value = { message: 'Välj motdrag', color: 'primary', icon: "mdi-information", buttonText: "Välj drag"}
-    submitButtonCallback = confirmVariationSaved
-  }
-}
-
-const playersTurn = computed(() => {
-  return turn.value.color == orientation.value
-})
-
-watchEffect(() => {
-  if (!playersTurn.value && movesData.value != undefined) {
-    selectedMove.value = getRandomMove(movesData.value.moves, movesToConsider)
-    boardAPI.move(selectedMove.value.san)
-    submitMove()
-  }
-})
-
-const undoMove = () => {
-  boardAPI.undoLastMove()
-}
 
 </script>
 
 <template>
   {{ userFeedback.message }}
-  <TheChessboard :board-config="boardConfig" @board-created="(api) => (boardAPI = api)"
-    @move="(move) => pieceMoved(move)" />
+  <TheChessboard :board-config="boardConfig" @board-created="(boardApi) => initGame(boardApi)"
+    @move="(move) => gameplay.pieceMoved(move)" />
 
   <div v-if="movesData/* && turn.color == orientation*/">
-    <v-btn v-for="move in movesData.moves" :key="move.san" @click="previewMove(move)" @mouseover="drawArrow(move)"
-      @mouseout="removeArrows()">{{ move.san }} {{ getTotalNumberOfGames(move) }}
+    <v-btn v-for="move in movesData.moves" :key="move.san" @click="gameplay.previewMove(move)"
+      @mouseover="board.drawMove(gameplay.getFrom(move), gameplay.getTo(move), 'green')" @mouseout="board.hideMoves()">{{
+        move.san }} {{ getTotalNumberOfGames(move) }}
     </v-btn>
-    <v-btn @click="submitButtonCallback">{{ userFeedback.buttonText }}</v-btn>
-    <v-btn @click="undoMove">Ångra drag</v-btn>
+    <v-btn @click="gameplay.submitButtonCallback()">{{ userFeedback.buttonText }}</v-btn>
+    <v-btn @click="board.undoLastMove()">Ångra drag</v-btn>
   </div>
 </template>
 
---
+++ src/types/Gameplay.ts
@@ -0,0 +1,143 @@
+import type { BoardApi, BoardConfig } from "vue3-chessboard";
+import type { Move } from "./Move";
+import { type MoveEvent } from "vue3-chessboard";
+import { useFetchMovesData } from "@/fetch";
+import { ref, watchEffect } from "vue";
+import { Turn } from "./Turn";
+import { getRandomMove } from "@/utils/utils";
+
+import type { Square } from "chess.js";
+import { Tree } from "./Tree";
+import type { UserFeedback } from "./UserFeedback";
+
+export class GameplayApi {
+  private turn = ref<Turn>(new Turn("white"));
+  private tree: Tree = new Tree();
+  private board: BoardApi | null = null
+  private orientation: BoardConfig["orientation"];
+  private selectedMove: Move | null = null;
+  private moveSequence = ref<string>("");
+  private movesData = useFetchMovesData(this.moveSequence);
+  private userFeedback = ref<UserFeedback>({
+    message: "Välj öppningsdrag",
+    color: "primary",
+    icon: "mdi-information",
+    buttonText: "Välj drag",
+  });
+
+  submitButtonCallback = this.submitMove;
+
+  constructor(orientation: BoardConfig["orientation"]) {
+    this.orientation = orientation;
+    watchEffect(() => {
+        if (this.turn.value.color != this.orientation && this.movesData.value != undefined) {
+          this.selectedMove = getRandomMove(this.movesData.value.moves, 2)
+          this.board?.move(this.selectedMove.san)
+          this.submitMove()
+        }
+      })
+  }
+
+  useGameplayData() {
+    return { movesData: this.movesData, userFeedback: this.userFeedback } 
+  }
+
+  setBoard(board: BoardApi) {
+    this.board = board
+  }
+
+  submitMove() {
+    this.tree.addMove(this.selectedMove!);
+    this.moveSequence.value = this.tree.getMoveSequence();
+    this.turn.value.toggle();
+    if (this.turn.value.color == this.orientation) this.determineState();
+  }
+
+  private resetBoard() {
+    this.board?.resetBoard();
+    this.turn.value = new Turn("white");
+    this.moveSequence.value = "";
+    this.submitButtonCallback = this.submitMove;
+    this.userFeedback.value = {
+      message: "Välj öppningsdrag",
+      color: "primary",
+      icon: "mdi-information",
+      buttonText: "Välj drag",
+    };
+    this.tree.resetMoveSequence();
+  }
+
+  private confirmVariationSaved() {
+    this.userFeedback.value = {
+      message: "Variant sparad",
+      color: "info",
+      icon: "mdi-check",
+      buttonText: "OK",
+    };
+    this.submitButtonCallback = this.submitAndReset;
+  }
+
+  private submitAndReset() {
+    this.tree.addMove(this.selectedMove!);
+    this.resetBoard();
+  }
+
+  private determineState() {
+    if (this.tree.hasMoves()) {
+      this.userFeedback.value = {
+        message: "Vilket drag spelar du nu?",
+        color: "info",
+        icon: "mdi-head-question",
+        buttonText: "Välj drag",
+      };
+      this.submitButtonCallback = this.guessMove;
+    } else {
+      this.userFeedback.value = {
+        message: "Välj motdrag",
+        color: "primary",
+        icon: "mdi-information",
+        buttonText: "Välj drag",
+      };
+      this.submitButtonCallback = this.confirmVariationSaved;
+    }
+  }
+
+  private guessMove() {
+    if (this.tree.hasNextMove(this.selectedMove!)) {
+      this.userFeedback.value = {
+        message: "Rätt!",
+        color: "success",
+        icon: "mdi-star-face",
+        buttonText: "Fortsätt",
+      };
+      this.submitButtonCallback = this.submitMove;
+    } else {
+      this.userFeedback.value = {
+        message: "Fel! Rätt drag var...",
+        color: "error",
+        icon: "mdi-alert",
+        buttonText: "Fortsätt",
+      };
+      this.submitButtonCallback = this.resetBoard;
+    }
+  }
+
+  pieceMoved(move: MoveEvent) {
+    this.selectedMove = this.movesData.value!.moves.filter(
+      (m) => m.san === move.san
+    )[0];
+  }
+
+  previewMove(move: Move) {
+    this.board?.move(move.san);
+    this.selectedMove = move;
+  }
+
+  getFrom(move: Move): Square {
+    return move.uci.slice(0, 2) as Square;
+  }
+
+  getTo(move: Move): Square {
+    return move.uci.slice(2, 4) as Square;
+  }
+}
